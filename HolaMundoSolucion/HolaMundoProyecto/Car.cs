c2966f835a5164b710d0bb074fa9b9dcd9f48724
Sergio Melero <sergiomele97@gmail.com>
2024-07-16 13:10:03 +0200

    Se realiza ejercicio coches

---
+++ HolaMundoSolucion/HolaMundoProyecto/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoProyecto
+{
+    internal class Car
+    {
+
+        public string Color {  get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string TipoMotor { get; set; }
+
+        private string matricula;
+
+        public Car(string color, string marca, string modelo, string tipoMotor, string matricula)
+        {
+            Color = color;
+            Marca = marca;
+            Modelo = modelo;
+            TipoMotor = tipoMotor;
+            this.matricula = matricula;
+        }
+
+        public override string ToString()
+        {
+            return $"Car: Marca = {Marca}, Modelo = {Modelo}, Color = {Color}, TipoMotor = {TipoMotor}, Matricula = {matricula}";
+        }
+    }
+}
--- HolaMundoSolucion/HolaMundoProyecto/Program.cs
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -6,6 +6,8 @@ namespace HolaMundoProyecto
     {
         static void Main(string[] args)
         {
+            // Ejercicio productos
+            Console.WriteLine("-----------------------------------------------------------\nEjercicio productos: \n");
 
             Producto Producto_A = new Producto("Producto_A", 100);
             Producto Producto_B = new Producto("Producto_B", 200);
@@ -25,6 +27,24 @@ namespace HolaMundoProyecto
                 total_ventas = total_ventas + p.ventas;
             }
             Console.WriteLine("El total de ventas es: " + total_ventas);
+
+
+            // Ejercicio coches
+            Console.WriteLine("-----------------------------------------------------------\nEjercicio coches: \n");
+            Car coche1 = new Car("Green", "Ford", "Mustang", "Gasoline", "12d1d23r");
+            Car coche2 = new Car("Red", "Toyota", "Prius", "Electric", "13d1113r");
+            Car coche3 = new Car("Blue", "Volskwagen", "Golf", "Deisel", "113423r");
+
+            List<Car> Car_list = new List<Car>();
+
+            Car_list.Add(coche1);
+            Car_list.Add(coche2);
+            Car_list.Add(coche3);
+
+            foreach (Car c in Car_list)
+            {
+                Console.WriteLine(c.ToString());
+            }
         }
     }
 }
