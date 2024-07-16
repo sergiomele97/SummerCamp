9311f7c772fd39bb403dc1f88d18da93a8567ab4
Alberto de León <alberto13590@hotmail.com>
2024-07-16 10:33:54 +0200

    inicio 16-07-2024

---
+++ HolaMundoSolucion/HolaMundoProyecto/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoProyecto
+{
+    internal class Producto
+    {
+        public string nombre = "";
+        public int ventas = 0;
+
+        public Producto(string nombre, int ventas) { 
+            
+            this.nombre = nombre;
+            this.ventas = ventas;
+        }
+    }
+}
---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HolaMundoProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Producto Producto_A = new Producto("Producto_A", 100);
+            Producto Producto_B = new Producto("Producto_B", 200);
+            Producto Producto_C = new Producto("Producto_C", 300);
+
+            List<Producto> Producto_List = new List<Producto>();
+
+            Producto_List.Add(Producto_A);
+            Producto_List.Add(Producto_B);
+            Producto_List.Add(Producto_C);
+
+            int total_ventas = 0;
+
+            foreach (Producto p in Producto_List)
+            {
+                Console.WriteLine(p.nombre);
+                total_ventas = total_ventas + p.ventas;
+            }
+            Console.WriteLine("El total de ventas es: " + total_ventas);
+        }
+    }
+}
